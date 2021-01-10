8d00e9b4d6cf87c2724855f8b0c18dba3237ec65
Sourav Ojha <sojha7724@gmail.com>
2021-01-10 19:14:42 +0530

    added ReverseArray.java file

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,28 +1,28 @@
-public class ReverseArray{
+public class ReverseArray {
     public static void main(String[] args) {
-		
-		int[] array = {1,2,3,4,5,6};
-		
-		reverseArray(array, 0,array.length-1);
-		printArray(array, array.length);
-		
-	}
-	
-	static void reverseArray(int array[],int start,int end ) {
-		int temp;
-		while(start<end) {
-			temp = array[start];
-			array[start] = array[end];
-			array[end] = temp;
-			start++;
-			end--;
-		}
-		
-}
-	static void printArray(int array[],int size) {
-		for(int i = 0;i<size;i++) {
-			System.out.print(array[i]+" ");
-		}
-	}
-}
 
+        int[] array = { 1, 2, 3, 4, 5, 6 };
+
+        reverseArray(array, 0, array.length - 1);
+        printArray(array, array.length);
+
+    }
+
+    static void reverseArray(int array[], int start, int end) {
+        int temp;
+        while (start < end) {
+            temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
+
+    static void printArray(int array[], int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
